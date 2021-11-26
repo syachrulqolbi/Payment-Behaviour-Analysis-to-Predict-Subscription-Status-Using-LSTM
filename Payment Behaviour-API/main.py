c182beb08ee6dafbc80ca206d4fc42fab1c4ab0c
Syachrul Qolbi <syachrulqolbinursepti@gmail.com>
2021-11-26 21:21:38 +0700

    Add files via upload

--- Payment Behaviour-API/main.py
+++ Payment Behaviour-API/main.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from pydantic import BaseModel
 import numpy as np
 import pickle
+import joblib
+from sklearn.preprocessing import StandardScaler
 import math
 
 app = FastAPI()
@@ -54,12 +56,14 @@ def encode_amountTotal(x):
 
 def encode_status(x):
     status = x
-    if (status != "PAID" or status != "UNPAID" or status != "ZERO BILLING") or status == "ZERO BILLING":
+    if status == "ZERO BILLING":
         status = 0
     elif status == "UNPAID":
         status = 1
     elif status == "PAID":
         status = 2
+    else:
+        status = 0
     return status
 
 def encode_paymentDate(x):
@@ -78,21 +82,44 @@ def encode_paket(x):
         paket[0] = 1
     return np.array([paket.astype(int)])
 
+def scaling_amountTotal(amountTotal_9, amountTotal_8, amountTotal_7, amountTotal_6, amountTotal_5, amountTotal_4, amountTotal_3, amountTotal_2, amountTotal_1):
+    x = np.array([amountTotal_9,
+                amountTotal_8,
+                amountTotal_7,
+                amountTotal_6,
+                amountTotal_5,
+                amountTotal_4,
+                amountTotal_3,
+                amountTotal_2,
+                amountTotal_1])
+    sc = joblib.load("scaler.gz")
+    return sc.transform(np.reshape(x, (1, -1)))
+
 @app.get("/")
 def home():
     return "Welcome"
 
 @app.get("/predict")
 def predict(data: Data):
-    data1 = np.array([[[encode_amountTotal(data.amountTotal_9), encode_status(data.status_9), encode_paymentDate(data.paymentDate_9)],
-                [encode_amountTotal(data.amountTotal_8), encode_status(data.status_8), encode_paymentDate(data.paymentDate_8)],
-                [encode_amountTotal(data.amountTotal_7), encode_status(data.status_7), encode_paymentDate(data.paymentDate_7)],
-                [encode_amountTotal(data.amountTotal_6), encode_status(data.status_6), encode_paymentDate(data.paymentDate_6)],
-                [encode_amountTotal(data.amountTotal_5), encode_status(data.status_5), encode_paymentDate(data.paymentDate_5)],
-                [encode_amountTotal(data.amountTotal_4), encode_status(data.status_4), encode_paymentDate(data.paymentDate_4)],
-                [encode_amountTotal(data.amountTotal_3), encode_status(data.status_3), encode_paymentDate(data.paymentDate_3)],
-                [encode_amountTotal(data.amountTotal_2), encode_status(data.status_2), encode_paymentDate(data.paymentDate_2)],
-                [encode_amountTotal(data.amountTotal_1), encode_status(data.status_1), encode_paymentDate(data.paymentDate_1)]]])
+    amount_total = scaling_amountTotal(encode_amountTotal(data.amountTotal_9), 
+                                    encode_amountTotal(data.amountTotal_8), 
+                                    encode_amountTotal(data.amountTotal_7), 
+                                    encode_amountTotal(data.amountTotal_6), 
+                                    encode_amountTotal(data.amountTotal_5), 
+                                    encode_amountTotal(data.amountTotal_4), 
+                                    encode_amountTotal(data.amountTotal_3), 
+                                    encode_amountTotal(data.amountTotal_2), 
+                                    encode_amountTotal(data.amountTotal_1))
+    
+    data1 = np.array([[[amount_total[0][0], encode_status(data.status_9), encode_paymentDate(data.paymentDate_9)],
+                [amount_total[0][1], encode_status(data.status_8), encode_paymentDate(data.paymentDate_8)],
+                [amount_total[0][2], encode_status(data.status_7), encode_paymentDate(data.paymentDate_7)],
+                [amount_total[0][3], encode_status(data.status_6), encode_paymentDate(data.paymentDate_6)],
+                [amount_total[0][4], encode_status(data.status_5), encode_paymentDate(data.paymentDate_5)],
+                [amount_total[0][5], encode_status(data.status_4), encode_paymentDate(data.paymentDate_4)],
+                [amount_total[0][6], encode_status(data.status_3), encode_paymentDate(data.paymentDate_3)],
+                [amount_total[0][7], encode_status(data.status_2), encode_paymentDate(data.paymentDate_2)],
+                [amount_total[0][8], encode_status(data.status_1), encode_paymentDate(data.paymentDate_1)]]])
     data2 = encode_paket(data.paketradius)
 
     data_test = [data1, data2]
@@ -109,6 +136,6 @@ def predict(data: Data):
     else:
         status = "Loyal"
     return {
-        "Percentage": round(float(result), 2) * 100,
+        "Percentage": round(float(result), 4) * 100,
         "Predict Description" : status
     }
